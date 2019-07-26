cd2d2fa06e7b800ebc573ab53bc37dbbff1da867
ATField2501 <atfield2501@gmail.com>
2019-07-26 20:29:55 +0200

    fichier secret d'identifiants

---
+++ secretsElephant.py
@@ -0,0 +1,9 @@
+# coding: utf8
+
+""" Identifiants secrets pour petit_elphant, le client Mastodon"""
+
+## Adresse mail relié à mastodon
+babar = "xx@xx"
+## Mot de passe pour compte mastodon
+babar_mdp = "mdp=^^"
+
